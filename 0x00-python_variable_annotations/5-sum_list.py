1e6ed52dddcd5bac266233ea8f46b2961c31e28c
achref <achrefbs>
2021-03-15 18:02:52 +0100

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+returns sum as a float.
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    returns sum as a float.
+    """
+    return float(sum(input_list))
