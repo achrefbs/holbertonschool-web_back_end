36ebdb7214b7393a3abb408f1f4412d577a3a6df
achref <achrefbs>
2021-03-15 18:34:05 +0100

    task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+square of the int/float
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    square of the int/float
+    """
+    return (k, float(v ** 2))
