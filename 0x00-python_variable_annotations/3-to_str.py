a89f8d663428db1788189c94650f84f12ea505f2
achref <achrefbs>
2021-03-15 17:44:24 +0100

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    string representation of the float.
+    """
+    return str(n)
