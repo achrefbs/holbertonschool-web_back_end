80c7de74b076525ae76661cb3e190dc78f184b6a
achref <achrefbs>
2021-03-15 17:02:43 +0100

    task 0 update

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,8 +3,8 @@ returns sum as a float
 """
 
 
-def add(x: float, y: float) -> float:
+def add(a: float, b: float) -> float:
     """
     returns sum as a float
     """
-    return x + y
+    return a + b
