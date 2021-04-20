ca3a49c4751bab991b610c2f986ff25539d0f709
achref <achrefbs>
2021-04-20 15:25:19 +0100

    task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+waits for a random delay between 0 and max_delay
+seconds and eventually returns it
+"""
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10):
+    """
+    waits for a random delay between 0 and max_delay
+    seconds and eventually returns it
+    """
+    i = random.uniform(0, max_delay)
+    await asyncio.sleep(i)
+    return i
