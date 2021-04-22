0862bc9dbd86da248ab8d2d2a8b776ae982aab0e
achref <achrefbs>
2021-04-22 13:36:20 +0100

    task 2 update

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -27,7 +27,7 @@ def get_locale():
     """
     determine the best match with our supported languages.
     """
-    return request.accept_languages.best_match(["en", "fr"])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
