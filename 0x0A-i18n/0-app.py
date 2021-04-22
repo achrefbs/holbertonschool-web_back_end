881d5ad8e8794ca4d1342ed1073929f2b3a08f6c
achref <achrefbs>
2021-04-22 11:58:24 +0100

    task 0 style

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,11 +1,22 @@
+#!/usr/bin/env python3
+"""
+a single / route and an index.html template that simply outputs
+“Welcome to Holberton” as page title (<title>) and “Hello world”
+as header (<h1>).
+"""
+
+
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """index"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
