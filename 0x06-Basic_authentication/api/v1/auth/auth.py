297a3f71d10b4aad70c161a1168fc5fb11d1ae73
achrefbs <siachrefboularess@gmail.com>
2021-06-17 22:35:10 +0100

    task 3

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+from flask import request
+from typing import List, TypeVar
+"""
+manage the API authentication.
+"""
+
+
+class Auth:
+    """
+    manage the API authentication.
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        require_auth
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        authorization_header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        current_user
+        """
+        return None
