cede032bc9518716631d2ac60aec2907dbc5ca61
achrefbs <siachrefboularess@gmail.com>
2021-06-17 15:01:58 +0100

    ?

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,14 +2,17 @@
 """
 filtered_logger
 """
+
 from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str):
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
     """
-    Returns the log message obfuscated
+    returns the log message obfuscated
     """
     for field in fields:
         message = re.sub(rf"{field}=.*?{separator}",
