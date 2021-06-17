a09313d382df0d0c19ca3666ff0592426fe2d782
achrefbs <siachrefboularess@gmail.com>
2021-06-17 15:11:29 +0100

    fix

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -15,7 +15,8 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
+        """init"""
         self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
 
